d8d6b02799fa558a6a25d53b8858ae045927ef26
Anil Kumar N <anilkumar15.1995@gmail.com>
2016-03-28 22:43:15 +0530

    RX to be updated

---
+++ ox_rtc.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import serial
+
+class ox_rtc:
+
+         ser = serial.Serial("/dev/ttyAMA0",9600)
+         tx=""
+         rx=""
+ 
+
+         def __init__(self):
+
+		tx = ":SETM001$$"
+		ser.write(tx.encode("hex"))
+		rx = ser.read()
+		return rx.decode("hex")
+
+
+	 def check_module_product_id(self):
+		
+		tx = ":A00507$$"
+		ser.write(tx.encode("hex"))
+		rx = ser.read()
+		return rx.decode("hex")
+
+           
+         def check_module_module_id(self):
+
+                tx = ":A003001$$"
+                ser.write(tx.encode("hex"))
+                rx = ser.read()
+                return rx.decode("hex")
+
+
+         def program_scene_events(self):
+
+                tx = ": PR 002 01 A01 B10021A $$"
+                ser.write(tx.encode("hex"))
+
+
+         def confirm_total_events(self):
+
+                tx = ": PR 002 01 C01$$"
+                ser.write(tx.encode("hex"))
+
+
+         def rtc_set_time_prog(self):
+
+                tx = ":PT003 01 M111111111111 D1111111 T131610$$"
+                ser.write(tx.encode("hex"))
+
+
+
+         def gate_set_time(self):
+
+                tx = ": GA250814145500$$"
+                ser.write(tx.encode("hex"))
+
+
+
+         def gate_req_time(self):
+
+                tx = ":GB000$$"
+                ser.write(tx.encode("hex"))
+		rx = ser.read()
+		return rx.decode("hex")
+
+
+
